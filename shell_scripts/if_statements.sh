68eb422b9664213415ce6a3e7ddac228132f181c
kibetstephanie <stephkiby@gmail.com>
2023-05-20 12:00:52 +0300

    Updated the if statements file

--- shell_scripts/if_statements.sh
+++ shell_scripts/if_statements.sh
@@ -1,12 +1,24 @@
 #! /usr/bin/bash
 
 read -p "What is your age? " AGE
+
+if [[ -z $AGE ]] #-z means if it's empty
+then 
+    echo "Input cannot be empty"
+    exit 1
+fi
+
+if ! [[ $AGE =~ ^[0-9]+$ ]] #All values should be a number
+then    
+    echo "Error: Age should be a number"
+    exit 1 #exits the input
+fi
 read -p "What is your name? " NAME
 
 if [[ $AGE == 18 ]]
 then
     echo "You are an adult"
-elif [[ $AGE -le 17 ]]
+elif [[ $AGE -le 17 ]] #-le means less than equal to
 then 
     echo "You're a minor"
 elif [[ $AGE == 18 && $NAME == "Cough" ]]
