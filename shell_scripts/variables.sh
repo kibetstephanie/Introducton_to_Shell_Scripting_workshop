a668d91d10580d2b42dd3878873cb3e357017206
kibetstephanie <stephkiby@gmail.com>
2023-05-20 11:16:11 +0300

    Created variables.sh

---
+++ shell_scripts/variables.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+#System Defined Variables
+echo "User: $USER"
+echo "Working directory: $PWD"
+
+#User Defined Variable
+AGE=18
+NAME="Stephanie"
+
+echo "My name is $NAME. I am $AGE years old."
